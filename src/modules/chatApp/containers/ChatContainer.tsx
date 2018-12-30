9159d95cb13d4e34a66ddb55691289bc1643bf97
Scott Daniel <scottberry91@gmail.com>
2018-12-30 22:21:20 +0000

    Turned chatcontainer into a function component

--- src/modules/chatApp/containers/ChatContainer.tsx
+++ src/modules/chatApp/containers/ChatContainer.tsx
@@ -22,58 +22,56 @@ interface IProps {
 	}
 }
 
-class ChatContainer extends React.Component<
+const ChatContainer: React.SFC<
 	ChildDataProps<IProps> &
 		RouteComponentProps<{ channel: string; team: string }>
-> {
-	render() {
-		const channelSlug = this.props.match.params.channel
+> = props => {
+	const channelSlug = props.match.params.channel
 
-		let channelId: string
-		let team: ShowTeamQuery_showTeam
+	let channelId: string
+	let team: ShowTeamQuery_showTeam
 
-		return (
-			// @ts-ignore
-			<ChatLayout>
-				<Query<ShowTeamQuery, ShowTeamQueryVariables>
-					query={SHOW_TEAM_QUERY}
-					variables={{ teamSlug: this.props.match.params.team }}
-				>
-					{({ data }) => {
-						if (data !== undefined) {
-							team = data.showTeam
+	return (
+		// @ts-ignore
+		<ChatLayout>
+			<Query<ShowTeamQuery, ShowTeamQueryVariables>
+				query={SHOW_TEAM_QUERY}
+				variables={{ teamSlug: props.match.params.team }}
+			>
+				{({ data }) => {
+					if (data !== undefined) {
+						team = data.showTeam
+					}
+					let authorId = ''
+					if (team !== undefined) {
+						authorId = team.author.id ? team.author.id : ''
+
+						if (team.channels !== null && team !== null) {
+							channelId = (team.channels.find(
+								channel => channel.slug === channelSlug
+							) as any).id
 						}
-						let authorId = ''
-						if (team !== undefined) {
-							authorId = team.author.id ? team.author.id : ''
 
-							if (team.channels !== null && team !== null) {
-								channelId = (team.channels.find(
-									channel => channel.slug === channelSlug
-								) as any).id
-							}
-
-							return team !== undefined ? (
-								<React.Fragment>
-									<TeamBar showTeam={team} {...this.props} />
-									{channelId !== undefined ? (
-										<ChatComponent channelId={channelId} />
-									) : null}
-									<MemberBar
-										channelId={channelId}
-										authorId={authorId}
-										{...this.props}
-										showTeam={team}
-									/>
-								</React.Fragment>
-							) : null
-						}
-						return null
-					}}
-				</Query>
-			</ChatLayout>
-		)
-	}
+						return team !== undefined ? (
+							<React.Fragment>
+								<TeamBar showTeam={team} {...props} />
+								{channelId !== undefined ? (
+									<ChatComponent channelId={channelId} />
+								) : null}
+								<MemberBar
+									channelId={channelId}
+									authorId={authorId}
+									{...props}
+									showTeam={team}
+								/>
+							</React.Fragment>
+						) : null
+					}
+					return null
+				}}
+			</Query>
+		</ChatLayout>
+	)
 }
 
 export default ChatContainer
